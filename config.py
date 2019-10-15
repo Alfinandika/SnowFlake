4866db846afeaf5f0ce3bcf72c277fd2b244b660
Alfin Andika Pratama <alfin_39@ymail.com>
2019-10-15 10:27:29 +0700

    edit config

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from app import app
 from flaskext.mysql import MySQL
 
 # Twitter configurations
-ACCESS_TOKEN = "289784806-oMOXkCEy2YS79TmtWYn8gYim2rprjD5FY2n2GPL0"
-ACCESS_TOKEN_SECRET = "6oqPSLtlC2ZAQWq9p9cuV53yP1mFL8NoTEAjyoZjVWTs1"
-CONSUMER_KEY = "8XG3Iu5OfQ9NeQS4rJDxD9bfh"
-CONSUMER_SECRET = "kc677CoU5jxapDK5cbldPVLDchkhyPTCDGKPsl1VlDz5PtXXxF"
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
  
 # MySQL configurations
 mysql = MySQL()
